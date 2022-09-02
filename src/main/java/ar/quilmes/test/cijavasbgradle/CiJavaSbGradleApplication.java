82cd7045c808c0fa24a94b46da7de928b4f1c488
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2022-09-02 16:06:22 -0300

    first commit

---
+++ src/main/java/ar/quilmes/test/cijavasbgradle/CiJavaSbGradleApplication.java
@@ -0,0 +1,13 @@
+package ar.quilmes.test.cijavasbgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiJavaSbGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiJavaSbGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/quilmes/test/cijavasbgradle/CiJavaSbGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.quilmes.test.cijavasbgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiJavaSbGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
